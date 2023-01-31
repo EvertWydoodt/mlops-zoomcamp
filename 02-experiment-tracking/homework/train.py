e70cbcc64543eceacdfa4d46c8c6a2449f0ee8b6
Evert Wydoodt <evert.wydoodt@gmail.com>
2023-01-31 10:18:55 +0000

    update les 2

--- 02-experiment-tracking/homework/train.py
+++ 02-experiment-tracking/homework/train.py
@@ -1,10 +1,13 @@
 import argparse
 import os
 import pickle
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+mlflow.set_experiment('random-forest-experiment')
+mlflow.sklearn.autolog()
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -13,15 +16,15 @@ def load_pickle(filename: str):
 
 def run(data_path):
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
+    with mlflow.start_run(): 
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
+        X_valid, y_valid = load_pickle(os.path.join(data_path, "valid.pkl"))
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_valid)
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_valid)
 
-    rmse = mean_squared_error(y_valid, y_pred, squared=False)
-
+        rmse = mean_squared_error(y_valid, y_pred, squared=False)
 
 if __name__ == '__main__':
 
